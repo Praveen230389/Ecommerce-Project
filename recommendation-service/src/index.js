996a7e0839239aa14b953db902dccf8f27634a39
Praveen230389 <Praveenkumar230389@gmail.com>
2026-07-23 16:26:24 +0530

    Update index.js

--- recommendation-service/src/index.js
+++ recommendation-service/src/index.js
@@ -10,5 +10,5 @@ router.get('/health', (req,res)=>res.status(200).json({status:'UP'})); router.ge
 app.use('/', router);
 app.use('/api/recommendation', router);
 
-const PORT = process.env.RECOMMENDATION_SERVICE_PORT || 3013;
+const PORT = process.env.PORT || 3013;
 app.listen(PORT, '0.0.0.0', () => console.log(`recommendation-service running on ${PORT}`));
