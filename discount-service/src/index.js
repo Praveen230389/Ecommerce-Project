360bc456d4affb31815baf435b062649435cac03
Praveen230389 <Praveenkumar230389@gmail.com>
2026-07-23 16:24:17 +0530

    Update index.js

--- discount-service/src/index.js
+++ discount-service/src/index.js
@@ -10,5 +10,5 @@ router.get('/health', (req,res)=>res.status(200).json({status:'UP'})); router.po
 app.use('/', router);
 app.use('/api/discount', router);
 
-const PORT = process.env.DISCOUNT_SERVICE_PORT || 3014;
+const PORT = process.env.PORT || 3014;
 app.listen(PORT, '0.0.0.0', () => console.log(`discount-service running on ${PORT}`));
