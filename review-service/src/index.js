ed4f918e86d5094c8712543e162378c326aff954
Praveen230389 <Praveenkumar230389@gmail.com>
2026-07-23 16:26:38 +0530

    Update index.js

--- review-service/src/index.js
+++ review-service/src/index.js
@@ -10,5 +10,5 @@ router.get('/health', (req,res)=>res.status(200).json({status:'UP'})); router.ge
 app.use('/', router);
 app.use('/api/reviews', router);
 
-const PORT = process.env.REVIEW_SERVICE_PORT || 3009;
+const PORT = process.env.PORT || 3009;
 app.listen(PORT, '0.0.0.0', () => console.log(`review-service running on ${PORT}`));
